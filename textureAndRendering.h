ef984973e77b2f595c0702326bf7184598e74a8a
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-08 11:50:35 +0100

    Update textureAndRendering.h

--- textureAndRendering.h
+++ textureAndRendering.h
@@ -16,6 +16,8 @@ typedef struct
 
 
 SDL_Texture *initTexture(const char *path, SDL_Renderer *renderer);
+Texture* loadTextureText(char* Text, SDL_Color textColor);
+void demolishTexture(Texture* texture);
 void render(const char *path, SDL_Renderer *renderer, int x, int y); // rendering texture in (x,y) plane
 int renderMenu(SDL_Renderer *renderer);
 void renderLogin(SDL_Renderer *renderer);
