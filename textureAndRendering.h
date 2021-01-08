cd9b7cf4e213e0e3c96a937b07ef47ff201096d4
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-08 11:04:56 +0100

    Create textureAndRendering.h

---
+++ textureAndRendering.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDl_image.h>
+#include <SDL_ttf.h>
+
+
+typedef struct
+{
+	SDL_Texture* texture;
+	char* path;
+	int width;
+	int height;
+}Texture;
+
+
+SDL_Texture *initTexture(const char *path, SDL_Renderer *renderer);
+void render(const char *path, SDL_Renderer *renderer, int x, int y); // rendering texture in (x,y) plane
+int renderMenu(SDL_Renderer *renderer);
+void renderLogin(SDL_Renderer *renderer);
+void renderSingup(SDL_Renderer *renderer);
