fe3b59e72e3b2c711d907f7d7021e9dca65e2fbf
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-09 17:36:42 +0100

    here we go again

--- gamedep.h
+++ gamedep.h
@@ -1,45 +1,44 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL.h>
 //Afin de collecter les info d' utlisateur comme input et crée un compte
-typedef struct 
+typedef struct
 {
-    char username[30];
-    char motcle[30];
-    double score;
+	char username[30];
+	char motcle[30];
+	double score;
 }signup;
 //En cree un compte qui memorise data d'utilisateur
-typedef struct 
+typedef struct
 {
-    char username[30];
-    char motcle[30];
-    double score;
+	char username[30];
+	char motcle[30];
+	double score;
 }login;
 //le status du jeu 
 struct game_status
 {
-    bool etat;
+	bool etat;
 };
 //parametre du menu du jeu
-typedef struct 
+typedef struct
 {
-    int click;
-    int select;
-    int input_state;
+	int click;
+	int select;
+	int input_state;
 }menu;
 //game parametre
-typedef struct 
+typedef struct
 {
-    int select;
-    int click;
-    int nombre_letter;
-    int nombre_time;
-    int score;
+	int select;
+	int click;
+	int nombre_letter;
+	int nombre_time;
+	int score;
 }game_parameters;
-int empty(char *path, char *mode) ;
-bool existe(login player) ;
-int Signup(signup user);
-int Login(login user);
-
-
+int empty(char *path, char *mode);
+bool existe(login player);
+bool Signup(signup user);
+bool Login(login user);
