2d2fddac9e4dfb51c5cd23e53dad16021a77ce40
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-25 01:06:46 +0100

    ADAPTING CODE TO GTK

--- src/menu.h
+++ src/menu.h
@@ -1,15 +1,45 @@
-#pragma once
-#include <stdbool.h> 
+#ifndef MENU_H
+#define MENU_H
+
 #include <stdio.h>
 #include <gtk/gtk.h>
+#include <stdlib.h>
 #include <string.h>
-    
-GtkBuilder* builder;
+#include<stdbool.h>
 
-GtkWidget* username;
-GtkWidget* password;
-FILE* fp=NULL;
 
+// GtkWidget* username;
+// GtkWidget* password;
+// typedef struct 
+// {
+//     GtkWidget* window;
+//     GtkWidget* window2;
+// }windows;
+
+
+extern GtkBuilder *builder;
+extern GtkWidget* username;
+extern GtkWidget* password;
+extern GtkWidget* usernameLogin;
+extern GtkWidget* passwordLogin;
+
+extern GtkWidget* window;
+extern GtkWidget* window2;
+extern GtkWidget* window3;
+extern GtkWidget* button6;
+extern GtkWidget* button1;
+extern GtkWidget* button2;
+extern GtkWidget* button3;
+extern GtkWidget* button4;
+extern GtkWidget* button5;
+extern GtkWidget* button7;
+
+extern FILE *fp;
+extern GtkWidget* usernameLogin;
+extern GtkWidget* passwordLogin;
+// extern signup *user;
+
+// extern login *userr;
 
 typedef struct
 {
@@ -25,22 +55,20 @@ typedef struct
         char username[30];
         char password[30];
         double score;
-        GtkWidget* SignUp;  
-          
-        
-}signup 
 
-typedef enum
-{
-        bool menuPhase;
-        bool gamePhase;
-}gameState;
+}signup;
 
-static void SignUp(GtkWiget* widget,gpointer data,signup s);
-          
-bool empty(char* nameFile,char* mode);
-
-bool exist(login user);
-
-bool isLogged(login usr);
-
+// typedef enum
+// {
+//         bool menuPhase;
+//         bool gamePhase;
+// }gameState;
+void LOGIN(GtkButton *button, gpointer data);
+void SIGNUP(GtkButton *button, gpointer data);
+int empty(char *filename, char *mode);
+void menu(void);
+void MENU(GtkButton *button, gpointer data);
+bool exist(login player,char* filename,char* mode);
+// void SignUp(GtkWidget *Widget, gpointer data);
+bool isSigned(login player);
+#endif
