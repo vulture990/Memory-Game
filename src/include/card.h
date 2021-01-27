68f15313a319e952277df9c8493434d0f836b710
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-27 04:31:53 +0100

    Update card.h

--- src/include/card.h
+++ src/include/card.h
@@ -1,1 +1,32 @@
+#ifndef CARD_H
+#define CARD_H
+// #include <stdio.h>
+// #include <gtk/gtk.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include<stdbool.h>
 
+// extern GtkBuilder* builder;
+// extern const char* cardID[16] = {"btn1","btn2","btn3","btn4","btn5","btn6","btn7","btn8","btn9","btn10","btn11","btn12","btn13","btn14","btn15","btn16"};
+// extern const char* backCardImagePath[16]={"rsc/img1.png","rsc/img2.png","rsc/img3.png","rsc/img1.png4","rsc/img5.png","rsc/img6.png","rsc/img7.png","rsc/img8.png","rsc/img1.png","rsc/img2.png","rsc/img3.png","rsc/img1.png4","rsc/img5.png","rsc/img6.png","rsc/img7.png","rsc/img8.png"};
+
+typedef struct card
+{
+
+    GtkWidget* button;
+    GtkWidget* frontImage;
+    GtkWidget* backImage;
+    bool showing;
+    void (*showCard)(struct card* self);
+    void (*flip)(struct card* self);
+}Card;
+Card* newCard(void);
+void showCard(struct card *card);
+void flip(struct card* card);
+Card* cardconstructor(struct card* c,GtkWidget* backCard,GtkWidget* front,GtkWidget* button);
+void mainCard(void);
+int get_index(GtkButton* btn);
+
+
+
+#endif
