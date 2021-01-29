41191ea19a69a63cadafd7ef562215dc06ee7091
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-29 15:05:45 +0100

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <string.h>
+#include<stdbool.h>
+// #include "menu.h"
+#include "card.h"
+
+int main(int argc,char* argv[])
+{
+    gtk_init(&argc,&argv);
+    // menu();
+    mainCard();
+    gtk_main();
+    return 0;
+}
