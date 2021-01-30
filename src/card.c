059539777498540d1a5c5b70a28aa82a4cdc6b8f
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-30 17:46:08 +0100

    now it s runs perfectly fine

--- src/card.c
+++ src/card.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include<stdbool.h>
-
 #include "card.h"
+// global var
 int j=0;
-
 const gchar*  cardID[16] = {"btn1","btn2","btn3","btn4","btn5","btn6","btn7","btn8","btn9","btn10","btn11","btn12","btn13","btn14","btn15","btn16"};
 gchar*  backCardImagePath[16]={"rsc/img1.png","rsc/img2.png","rsc/img3.png","rsc/img4.png","rsc/img5.png","rsc/img6.png","rsc/img7.png","rsc/img9.png","rsc/img1.png","rsc/img2.png","rsc/img3.png","rsc/img4.png","rsc/img5.png","rsc/img6.png","rsc/img7.png","rsc/img9.png"};
 GtkWidget* buttons[16];
@@ -14,7 +13,7 @@ GtkWidget* images[16];
 GtkWidget* imagesFront[16];
 struct pairCard* pcard=(struct pairCard *)malloc(sizeof(struct pairCard));
 Card* card=newCard();
-
+//defining
 Card* newCard(void)//always remeber to release the memory space after you are done
 {
     Card* a=(Card* )malloc(sizeof(Card));
@@ -202,14 +201,19 @@ static void clickButton(GtkButton*button,gpointer data)// we r gonna pass addres
     }while(!pcard->numberOfclicks);
 
 }
-void mainCard(void)
+void mainCard()
+
 {
 
     GtkBuilder* builder;
+    GtkBuilder* builderr;
+
    // card=cardconstructor(card,,"./rsc/pokemon.p);
     // we must first click the button then show
 
     // flipCard(card);
+    builderr=gtk_builder_new_from_file ("./src/memorygame2.glade");
+
     pcard->cardMatch1=(char *)malloc(50*sizeof(char));
     pcard->cardMatch2=(char *)malloc(50*sizeof(char));
     pcard->cardMatch1=NULL;
@@ -217,12 +221,13 @@ void mainCard(void)
     pcard->card_1=newCard();
     pcard->card_2=newCard();
     GtkWidget* window;
-    builder=gtk_builder_new_from_file ("./rsc/glade/gameboard.glade");
+    builder=gtk_builder_new_from_file ("./src/gameboard.glade");
     window=GTK_WIDGET(gtk_builder_get_object(builder,"gameWindow"));
     for(int i=0;i<16;i++)
     {
         buttons[i]=GTK_WIDGET(gtk_builder_get_object(builder,cardID[i]));
     }
+    shuffling();
     for(int i=0;i<16;i++)
     {
         images[i]=gtk_image_new_from_file(backCardImagePath[i]);
@@ -243,185 +248,23 @@ void mainCard(void)
 }
 
 
-// typedef struct card
-// {
-//     int front;     
-//     char* backCard;
-//     bool showing;
-//     void (*showCard)(struct card* self);
-//     void (*flip)(struct card* self);
-// }Card;
-// typedef struct memoryGame
-// { 
-//     const char* words[8]={"Bleuu","Bleuu","Yellow","Yellow","Black","Black","White","White"}; 
-//     Card borad[4][4];//4x4 matrice
-//     int r=rand();
-//     void (*setCells)(struct memoryGame* m);
-//     void (*printCells)(struct memoryGame* m);
-//     void (*shuffle)(struct memoryGame* m);
-//     void (*matchingPairs)(struct memoryGame* m);
-
-// }MemoryGame;
-
-// Card* newCard();
-// Card* cardConstructor(struct card* card,char* BACKCARD,int FRONT);
-// void showCard(struct card *card);
-// void flipCard(struct card* card);
-// void memoryGameConstr(struct memoryGame* m);
-// void shuffle(struct memoryGame* m);
-// void setCells(struct memoryGame* m);
-// void printCells(struct memoryGame*m);
-// void matching (struct memoryGame* m);
-// Card* newCard2(struct card c);
-
-
-// int main(void)
-// {
-//     struct memoryGame* m=(struct memoryGame*)malloc(sizeof(MemoryGame));
-//     memoryGameConstr(m);
-//     return 0;
-// }
-
-// Card* newCard2(struct card c)
-// {
-//         Card* a=newCard();
-//         a=&c;
-//         return a;
-// }
-
-
-// Card* newCard()
-// {
-//     Card* a=(Card* )malloc((sizeof(Card)));
-//     return a;
-// }
-
-// void showCard(Card *card)
-// {
-//     if(card->showing)
-//     {
-//         printf("%s ",card->backCard);
-//     }
-//     else
-//     {
-//         printf(" [ %d ] ",card->front);
-//     }
-// }
-
-// void flipCard(Card* card)
-// {
-//     if(card->showing)
-//     {
-//         card->showing=false;
-//     }
-//     else
-//     {
-//         card->showing=true;
-//     }
-// }
-
-
-// Card* cardConstructor(struct card* card,char* BACKCARD,int FRONT)
-// {
-//     card->front=FRONT;
-//     card->backCard=BACKCARD;
-//     card->showing=false;//which simply means all cards are turned down by default
-//     // card->showCard=showCard;
-//     // card->flip=flipCard;
-//     return card;
-// } 
-
-// void setCells(MemoryGame* m)
-// {
-//     Card* c=newCard();
-
-         
-//     int i =0;
-//     for(int row=0;row<4;row++)
-//     {
-//         for(int col=0;col<4;col++)
-//         {
-//             char*s;
-//             strcpy(s,m->words[i])
-//             c=cardConstructor(newCard2(m->borad[row][col]),s,i);
-//             m->borad[row][col]=*c;
-                
-//             i++;
-//         }
-//     }
-// }
-
-// void printCells(MemoryGame* m)
-// {
-//     Card a;
-//     for(int row=0;row<4;row++)
-//     {
-//         for(int col=0;col<4;col++)
-//         {
-//             a=m->borad[row][col];
-//             showCard(&a);
-//         }
-//         printf("\n");
-//     }
-
-// }
-
-
-// void shuffle(struct memoryGame*m)
-// {
-//     for(int i=0;i<8;i++)
-//     {
-//         int pos=(m->r)%8;
-//         char* temp;
-//         strcpy(temp,m->words[i]);
-//         strcpy(m->words[i],m->words[pos]);
-//         strcpy(m-words[pos],temp);
-//     }
-    
-// }
-
-// void matching(MemoryGame* m)
-// {
-//     int cardChoice,row1,col1,row2,col2;
-//     printf("enter the number of card.");
-//     printf("\nFirst Card Choice?");
-
-//     scanf("%d",&cardChoice);
-//     row1=cardChoice/4;
-//     col1=cardChoice%4;
-//     flipCard(newCard2(m->borad[row1][col1]));
-//     printf("pick an other number?");
-//     printf("\nSecond Card Choice");
-//     scanf("%d",&cardChoice);
-//     row2=cardChoice/4;
-//     col2=cardChoice%4;
-//     flipCard(newCard2(m->borad[row2][col2]));
-//     getchar();
-//     printCells(m);
-    
-// }
-// void memoryGameConstr(struct memoryGame* m)
-// {
-//     for(int i=0;i<4;i++)
-//     {  
-//         for(int j=0;j<4;j++)
-//         {
-//              Card* c=newCard();
-//              Card cc=*c;
-//              (m->borad[i][j])=cc;
-//              free(c);
-//         }
-
-//     }
-//     // shuffle and setcells and printcells  last but not least play the game
-//     shuffle(m);
-//     setCells(m);
-//     printCells(m);
-//     matching(m);
-// }
-
-
-
+void shuffling()
+{
+    int i=0;
+    // create a random numbers generator
+    GRand * generator;
+    generator = g_rand_new ();
+    int random1,random2;
+    for (i=0;i<200;i++)
+    {
+        random1 = g_rand_int_range(generator,0,12);
+        random2 = g_rand_int_range(generator,0,12);
+        // shuffling buttons_str
+        gchar* inter2 = backCardImagePath[random1];
+        backCardImagePath[random1] = backCardImagePath[random2];
+        backCardImagePath[random2] = inter2;
+    }
+}
 
 
 
