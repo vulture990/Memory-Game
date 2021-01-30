88e4e8416641a1a9a663d144147a3d68d6e35767
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-01-30 06:25:35 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include<stdbool.h>
-// #include "menu.h"
+#include "menu.h"
 #include "card.h"
 
 int main(int argc,char* argv[])
 {
     gtk_init(&argc,&argv);
-    // menu();
-    mainCard();
+    menu();
+    //mainCard();
     gtk_main();
     return 0;
 }
