773d7eb15131b84d126cbb288babc353ffb62e4c
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-02-07 21:01:54 +0100

    Update card.c

--- card.c
+++ card.c
@@ -6,8 +6,6 @@
 #include "login.h"
 #include "card.h"
 
-
-
 // global var
 int j=0;
 const gchar*  cardID[16] = {"btn1","btn2","btn3","btn4","btn5","btn6","btn7","btn8","btn9","btn10","btn11","btn12","btn13","btn14","btn15","btn16"};
